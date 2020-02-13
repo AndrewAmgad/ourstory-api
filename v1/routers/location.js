d23eda7947e3824ada71a144e87c82135f57d33f
Andrew Amgad <andrewamgad30@gmail.com>
2020-02-13 15:57:06 +0200

    Bug fixes

--- v1/routers/location.js
+++ v1/routers/location.js
@@ -8,43 +8,19 @@ let data = JSON.parse(file);
 const errorResponse = require('../helper-functions').errorResponse;
 
 router.get('/countries', (req, res, next) => {
-    // var countries = []
-
-    // // push all countries to a single array
-    // for(var i = 0; i < data.length; i++){
-    //     countries.push({id: data[i].id, country: data[i].country});
-    // };
-
-    // res.status(200).json(countries);
-
     var countries = []
 
+    // push all countries to a single array
     for(var i = 0; i < data.length; i++){
-        countries.push(data[i]);
-    }
-
-    for(var i = 0; i < countries.length; i++){
-        countries[i].name = countries[i].country;
-        countries[i].cities = countries[i].states
-        delete countries[i].states
-        delete countries[i].country;
-
-        for(var j = 0; j < countries[i].cities.length; j++){
-            countries[i].cities[j].name = countries[i].cities[j].state
-            delete countries[i].cities[j].state
-        };
+        countries.push({id: data[i].id, name: data[i].name});
     };
 
-    fs.writeFileSync('cities.json', JSON.stringify(countries) , function (err) {
-        if (err) throw err;
-        console.log('Replaced!');
-      });
-
-    res.status(200).json(countries)
+    res.status(200).json(countries);
 });
 
 router.get('/cities', (req, res, next) => {
     const countryId = req.body.country_id;
+    console.log(countryId)
     
     if(!countryId) return errorResponse(res, 400, "Country ID must be provided")
 
