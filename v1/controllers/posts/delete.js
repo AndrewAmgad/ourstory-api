a051eb082cc947380d66fa1104c7b99313474251
Andrew Amgad <andrewamgad30@gmail.com>
2020-02-19 10:02:44 +0200

    Bug fixes

--- v1/controllers/posts/delete.js
+++ v1/controllers/posts/delete.js
@@ -1,13 +1,16 @@
 const Post = require('../../models/post');
 
 const errorResponse = require('../../helper-functions').errorResponse;
+var mongoose = require('mongoose');
 
 module.exports = deletePost = (req, res, next) => {
     const postId = req.params.post_id;
     const userId = req.userData.userId;
 
-    Post.findById(postId).then(post => {
+    if(!mongoose.Types.ObjectId.isValid(postId)) return errorResponse(res, 400, "Invalid post ID")
 
+    Post.findById(postId).then(post => {
+        if(!post) return errorResponse(res, 404, "Invalid post ID");
         // return an error if the user's ID does not match the post author's
         if(post.author_id !== userId) return errorResponse(res, 401, "Unauthorized");
 
