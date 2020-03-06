bf3ba4aff45ec72e13179e20aeb87592d14e2a72
Andrew Amgad <andrewamgad30@gmail.com>
2020-03-06 10:42:59 +0200

    Modified the delete post endpoint

--- v1/controllers/posts/delete.js
+++ v1/controllers/posts/delete.js
@@ -1,5 +1,6 @@
 const Post = require('../../models/post');
 const Comment = require('../../models/comment');
+const Notification = require('../../models/notification');
 
 const errorResponse = require('../../helper-functions').errorResponse;
 var mongoose = require('mongoose');
@@ -11,18 +12,20 @@ module.exports = deletePost = (req, res, next) => {
     // validate provided post ID
     if(!mongoose.Types.ObjectId.isValid(postId)) return errorResponse(res, 400, "Invalid post ID")
 
-    Post.findById(postId).then(post => {
+    Post.findById(postId).then(async post => {
         if(!post) return errorResponse(res, 404, "Invalid post ID");
         // return an error if the user's ID does not match the post author's
         if(post.author_id !== userId) return errorResponse(res, 401, "Unauthorized");
 
         // delete the post's comments
-        Comment.deleteMany({post_id: postId}).then(() => console.log("comments deleted"))
-        .catch(err => errorResponse(res, 500, err.message));
+        await Comment.deleteMany({post_id: postId});
+
+        // delete all notifications related to the post
+        await Notification.deleteMany({post_id: postId});
 
         Post.findByIdAndRemove(postId).then((post) => {
             res.status(200).json({message: "Post deleted successfully"})
-        }).catch(err => errorResponse(res, 500, err.message));
+        })
         
     }).catch(err => errorResponse(res, 500, err.message));
 };
