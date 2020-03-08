f77aa42bbb1b1498f0b404d905b4847db166be6c
Andrew Amgad <andrewamgad30@gmail.com>
2020-03-08 21:33:25 +0200

    Bug fixes

--- v1/controllers/notifications/settings.js
+++ v1/controllers/notifications/settings.js
@@ -4,24 +4,27 @@ const errorResponse = require('../../helper-functions').errorResponse
 // sendpoint for enabling / disabling notifications for the user
 module.exports.updateSettings = async (req, res, next) => {
     const userId = req.userData.userId;
-    
+
     // userActivity will be used to enable notifications of posts the user commented at
     // userPosts is notifications of the user's posts
     const userActivity = req.body.user_activity;
     const userPosts = req.body.user_posts
 
-    if(!userActivity && !userPosts) return errorResponse(res, 400, "One of the following fields are required 'user_activity', 'user_posts'");
+    if (!userActivity && !userPosts) return errorResponse(res, 400, "One of the following fields are required 'user_activity', 'user_posts'");
 
     const user = await User.findById(userId).catch(err => (errorResponse(res, 500, err.message)));
 
     // get the user's previous settings and apply only the changes.
     var notificationSettings = user.notificationSettings ? user.notificationSettings : {};
-    if(userActivity !== undefined) notificationSettings.userActivity = userActivity;
-    if(userPosts !== undefined) notificationSettings.userPosts = userPosts;
+    if (userActivity !== undefined) notificationSettings.userActivity = userActivity;
+    if (userPosts !== undefined) notificationSettings.userPosts = userPosts;
 
     // update the user notification settings using the updated settings object.
-    User.findByIdAndUpdate(userId, {notificationSettings: notificationSettings}, {new: true}).then((user) => {
-        res.status(200).json(user.notificationSettings);
+    User.findByIdAndUpdate(userId, { notificationSettings: notificationSettings }, { new: true }).then((user) => {
+
+        const { notificationSettings } = user;
+        return res.status(200).json({ user_activity: notificationSettings.userActivity, user_posts: notificationSettings.userPosts });
+
     }).catch(err => (errorResponse(res, 500, err.message)));
 };
 
@@ -29,6 +32,8 @@ module.exports.getSettings = (req, res, next) => {
     const userId = req.userData.userId;
 
     User.findById(userId).then(user => {
-        return res.status(200).json(user.notificationSettings);
-    });
+
+        const { notificationSettings } = user;
+        return res.status(200).json({ user_activity: notificationSettings.userActivity, user_posts: notificationSettings.userPosts })
+})
 }
